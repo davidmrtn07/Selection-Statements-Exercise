09f16c6be5523e94afb90b0085b0ae577f23f766
davidmrtn07 <davidmrtn07@gmail.com>
2024-03-16 08:31:13 -0500

    name

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿
+
+namespace SelectionStatements 
+{ 
+public class Program
+{
+    static void Main(string[] args)
+    {
+        //Number Guessing Game
+        //var r = new Random();
+        //var favNumber = r.Next(1, 1000);
+
+        //Console.WriteLine("Pick a number from 1 - 1000");
+        //var userInput = int.Parse(Console.ReadLine());
+
+        //if(userInput < favNumber)
+        //{ 
+        //    Console.WriteLine($" {userInput} is too low");
+        //}
+        //else if(userInput > favNumber) 
+        //{ 
+        //    Console.WriteLine($"{userInput} is too high");
+        //}
+        //else
+        //{
+        //    Console.WriteLine("You guessed it!");
+
+        //Switch-Case
+        Console.WriteLine("What is Your favorite Subject?");
+
+        string subject = Console.ReadLine();
+
+        switch (subject.ToLower())
+        {
+            case "math":
+                Console.WriteLine("I hate Math!");
+                break;
+            case "science":
+                Console.WriteLine("I love Science!");
+                break;
+            case "pe":
+                Console.WriteLine("PE is the best!!");
+                break;
+            case "history":
+                Console.WriteLine("History is fun!");
+                break;
+            case "english":
+                Console.WriteLine("English is boring.");
+                break;
+            default:
+                Console.WriteLine("I don't know that subject.");
+                break;
+
+
+        }
+        
+
+    }
+
+
+
+}
+}
